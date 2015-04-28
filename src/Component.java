a1e1a56111fcfb56724bd250731a03b84fea6908
Linus Sjöström <linus_sjostrom@hotmail.com>
2015-04-28 19:04:37 +0200

    Pär och company
    Ändringar i Main, Game och lagt till Component

---
+++ src/Component.java
@@ -0,0 +1,98 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
+import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+
+
+public class Component extends JComponent {
+	private Game game;
+	private BufferedImage player;
+	
+	public static final int HEIGHT = 720;
+	public static final int WIDTH = 1280;
+
+	public Component(Game game) {
+        this.game = game;
+        setInput();
+        
+        try {
+            player = ImageIO.read(new File(FileSystems.getDefault().getPath(
+                    "data", "stickman.jpg").toUri()));
+        } catch (IOException e) {
+            System.out.println("Image not found");
+        }
+     }
+	
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(WIDTH, HEIGHT);
+    }
+	
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0, 0, WIDTH, HEIGHT);
+        g2.drawImage(player, 200, 500, 120, 250, null);
+    }
+    public enum Direction {
+        UP, LEFT, RIGHT
+    }
+    
+	private void setInput() {
+		// TODO Auto-generated method stub
+	    getInputMap().put(KeyStroke.getKeyStroke("UP"), "UP");
+        getActionMap().put("UP", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.move(Direction.UP, true);
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke("released UP"), "no UP");
+        getActionMap().put("no UP", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.move(Direction.UP, false);
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "LEFT");
+        getActionMap().put("LEFT", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.move(Direction.LEFT, true);
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke("released LEFT"), "no LEFT");
+        getActionMap().put("no LEFT", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.move(Direction.LEFT, false);
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "RIGHT");
+        getActionMap().put("RIGHT", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.move(Direction.RIGHT, true);
+            }
+        });
+        getInputMap().put(KeyStroke.getKeyStroke("released RIGHT"), "no RIGHT");
+        getActionMap().put("no RIGHT", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.move(Direction.RIGHT, false);
+            }
+        });
+	}
+}
--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,79 @@
+import java.awt.BorderLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.swing.*;
 
-public class Game {
+
+public class Game extends JFrame {
 	
 	private Set<Node> nodes;
-	private Physics physics;
+	private double lastUpdate;
 	
+	boolean movingLeft;
+	boolean movingRight;
+	boolean movingUp;
+
+
 	public Game()
 	{
-		this.nodes = new HashSet<Node>();
-		this.physics = new Physics(9.82);
+		super("Fysikspel");
+		this.movingUp = false;
+        this.movingLeft = false;
+        this.movingRight = false;
 	}
+	
+	public void run(){
+        Component component = new Component(this);
+        lastUpdate = System.currentTimeMillis();
+        
+        this.setLayout(new BorderLayout());
+        this.add(component, BorderLayout.CENTER);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        
+        AbstractAction doOneStep = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double currentTime = System.currentTimeMillis();
+                update(currentTime - lastUpdate);
+                lastUpdate = currentTime;
+                component.repaint();
+            }
+        };
+
+        Timer timer = new Timer(17, doOneStep);
+        timer.setCoalesce(true);
+        timer.start();
+
+
+        super.setVisible(true);
+	}
+	
+	 public void update(double updateTime) {
+	    	//System.out.println("bla");
+	    }
 	
 	public boolean addNode(Node n)
 	{
 		return this.nodes.add(n);
 	}
+    public void move(Component.Direction dir, boolean val) {
+        switch (dir){
+            case UP:
+                movingUp = val;
+                System.out.println("Up");
+                break;
+            case LEFT:
+                movingLeft = val;
+                System.out.println("Left");
+                break;
+            case RIGHT:
+                movingRight = val;
+                System.out.println("Right");
+                break;
+        }
+    }
 }
